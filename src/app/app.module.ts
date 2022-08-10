4ffd8b2abba55cef96241133967782c3cac92433
namitrathod1 <namit.rathod@cognizant.com>
2022-08-10 16:22:36 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   imports: [
@@ -30,12 +29,13 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatDividerModule,
     MatProgressBarModule,
-    FlexLayoutModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    HeaderComponent,
+    ProductsComponent,
   ],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -4,17 +4,15 @@ import { ProductsdetailsService } from '../service/productsdetails.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
   products: any;
-  constructor(private productdetail: ProductsdetailsService) { }
+  constructor(private productdetail: ProductsdetailsService) {}
 
-  ngOnInit() {
-    this.productdetail.getproducts().subscribe((data: any)=>{this.products=
-      (data as any).products})
+  ngOnInit(): void {
+    this.productdetail.getproducts().subscribe((data: any) => {
+      this.products = (data as any).products;
+    });
   }
-
-  }
-
 }
